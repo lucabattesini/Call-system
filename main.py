e6bf17b0014ac1adab19cfc44b9f16aa6d365ccb
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-08-21 11:42:28 -0300

    saving a string in a csv file with streamlit

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import streamlit as st
+
+yn = st.number_input("Write 1 or 0")
+
+if yn == 1:
+    with open("bfotool-download(1).csv", 'a') as txt_file:
+            txt_file.write('1')
