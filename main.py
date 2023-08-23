cb3c604a6f00e3ef75075b9da129d0bf9de49f3d
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-08-23 10:29:30 -0300

    reading and printing the csv file

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 yn = st.number_input("Write 1 or 0")
 
-if yn == 1:
-    with open("bfotool-download(1).csv", 'a') as txt_file:
-            txt_file.write('1')
+df = pd.read_csv("./bfotool-download(1).csv")
+
+print(df)
