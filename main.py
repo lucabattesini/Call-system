04d35b342a4405f0f9fcba1d13f5bbefa27b49bc
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-08-24 11:17:02 -0300

    Doing a simple call system

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("./bfotool-download(1).csv")
+#df = pd.read_csv("./bfotool-download(1).csv")
 
 dados = {'nome': ["Luca","Clara","Nick"],
-         'veio': ["he", "lo", "di"]}
+         'veio': [0, 0, 0]}
 
 dfe = pd.DataFrame(dados)
+
+st.markdown("### Luca")
+if st.button("Veio"):
+    dfe.loc[dfe['nome'] == 'Luca', 'veio'] = 1
+
+st.markdown("### Clara")
+if st.button("Veio?"):
+    dfe.loc[dfe['nome'] == 'Clara', 'veio'] = 1
+
+st.markdown("### Nick")
+if st.button("Veio??"):
+    dfe.loc[dfe['nome'] == 'Nick', 'veio'] = 1
+
 
 print(dfe)
