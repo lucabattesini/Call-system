1c478ddcd71b275035759a59c03be7b401c30e61
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-01 10:00:17 -0300

    Making the program save lola, clara and nick in the csv file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 
 df = pd.read_csv("./dados.csv")
+df.head()
 
 #dados = {'nome': ["Luca","Clara","Nick"],
          #'veio': [0, 0, 0]}
@@ -11,18 +12,25 @@ st.markdown("### Luca")
 if st.button("Veio"):
     df.loc[df['nome']=='luca', ['presenca']] = 1
     print(df)
-    df.to_csv('./dados.csv')
+    df.to_csv('./dados.csv', index=False)
     #df.at['luca', 'presenca'] = 1
 
 st.markdown("### Clara")
 if st.button("Veio?"):
-    df.loc[['clara'], ['presenca']] = 1
-    df.to_csv('./dados.csv')
+    df.loc[df['nome']=='clara', ['presenca']] = 1
+    print(df)
+    df.to_csv('./dados.csv', index=False)
 
 st.markdown("### Nick")
 if st.button("Veio??"):
-    df.loc[['yannick'], ['presenca']] = 1
-    df.to_csv('./dados.csv')
+    df.loc[df['nome']=='yannick', ['presenca']] = 1
+    print(df)
+    df.to_csv('./dados.csv', index=False)
 
+st.markdown("### Lola")
+if st.button("Veio???"):
+    df.loc[df['nome']=='lola', ['presenca']] = 1
+    print(df)
+    df.to_csv('./dados.csv', index=False)
 
 print(df)
