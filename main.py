f002d4cb20b1eb68d6870ffcbe76a046cb3ecf3d
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-06 16:25:00 -0300

    using the student id to save the call list

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Importing pandas and streamlit
 import pandas as pd
 import streamlit as st
 from datetime import datetime
@@ -12,18 +11,21 @@ x = int(1)
 data = datetime.now()
 fdata = data.strftime('%d/%m/%y')
 
-for name in df2['first_name']:
-    
-    st.markdown(f"### {name}")
-    
+for index, row in df2.iterrows():
+    id = row['student_id']
+    first_name = row['first_name']
+    last_name = row['last_name']
+
+    st.markdown(f"### {first_name} {last_name}")
+
     x = x + 1
-    
     if st.button("Veio?", key=x):
-        df2.loc[df2['first_name']==f'{name}', ['attendance_total']] = 1
-        df2.loc[df2['first_name']==f'{name}', ['date']] = fdata
+        print(id)
+        df2.loc[df2['student_id']==id, 'attendance_total'] = 1
+        df2.loc[df2['student_id']==id, 'date'] = fdata
         dftosave = df2[['student_id', 'attendance_total', 'date']]
-                
-        df2.to_csv('./call_list_students_utf-8.csv', index=False)
-        dftosave.to_csv('./attendance_table.csv', index=False)
-print(df)
-print(df2)
+        print(dftosave)
+df2.to_csv('./call_list_students_utf-8.csv', index=False)
+dftosave.to_csv('./attendance_table.csv', index=False)
+#print(df)
+#print(df2)
