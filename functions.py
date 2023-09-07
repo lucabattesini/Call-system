0e107e60a5ca9988040c4b9aba77c8163e542f84
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-07 17:14:27 -0300

    turning the for in into a function

--- functions.py
+++ functions.py
@@ -2,3 +2,25 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime
 
+def call_function(df, df2, x, x1, fdata)  :
+    for index, row in df2.iterrows():
+        id = row['student_id']
+        first_name = row['first_name']
+        last_name = row['last_name']
+
+        st.markdown(f"### {first_name} {last_name}")
+
+        x = x + 1
+        x1 = x1 + 'b'
+        if st.button("Presente", key=x):
+            df.loc[index] = {'student_id': id, 'attendance_total': 1, 'date': fdata}
+            df2.loc[df2['student_id']==id, 'attendance_total'] += 1
+            df2.loc[df2['student_id']==id, 'date'] = fdata
+            df.to_csv('./attendance_table.csv', index=False)
+
+        if st.button("Ausente", key=x1):
+            df.loc[index] = {'student_id': id, 'attendance_total': 0, 'date': fdata}
+            df2.loc[df2['student_id']==id, 'attendance_total'] += 0
+            df2.loc[df2['student_id']==id, 'date'] = fdata
+            df.to_csv('./attendance_table.csv', index=False)
+    df2.to_csv('./call_list_students_utf-8.csv', index=False)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from datetime import datetime
+from functions import call_function
  
 df = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
 df2 = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
@@ -12,28 +13,7 @@ x1 = 'a'
 data = datetime.now()
 fdata = data.strftime('%d/%m/%y')
 
-for index, row in df2.iterrows():
-    id = row['student_id']
-    first_name = row['first_name']
-    last_name = row['last_name']
-
-    st.markdown(f"### {first_name} {last_name}")
-
-    x = x + 1
-    x1 = x1 + 'b'
-    if st.button("Presente", key=x):
-        df.loc[index] = {'student_id': id, 'attendance_total': 1, 'date': fdata}
-        df2.loc[df2['student_id']==id, 'attendance_total'] += 1
-        df2.loc[df2['student_id']==id, 'date'] = fdata
-        df.to_csv('./attendance_table.csv', index=False)
-
-    if st.button("Ausente", key=x1):
-        df.loc[index] = {'student_id': id, 'attendance_total': 0, 'date': fdata}
-        df2.loc[df2['student_id']==id, 'attendance_total'] += 0
-        df2.loc[df2['student_id']==id, 'date'] = fdata
-        df.to_csv('./attendance_table.csv', index=False)
-
-df2.to_csv('./call_list_students_utf-8.csv', index=False)
+call_function(df, df2, x, x1, fdata)
 
 #print(df)
 #print(df2)
