e7fb66ba8e5b9e1bdffe3dd015fb3d0792be7f13
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-11 10:30:36 -0300

    creating a function to sum attendance total

--- functions.py
+++ functions.py
@@ -16,5 +16,18 @@ def compute_attendance(attendance, student_list, fdata):
         if st.button("Ausente", key=f'{index}_Ausente'):
             row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [fdata]})
             attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
-        
+
     attendance.to_csv('./attendance_table.csv', index=False)
+
+def call_list_sum(attendance, student_list):
+    for i, row in student_list.iterrows():
+        id = row['student_id']
+        x = 0
+        for index, row2 in attendance.iterrows():
+            id2 = row2['student_id']
+            value = row2['attendance']
+            if id == id2:
+                if value == 1:
+                    x = x + 1
+        
+        print(x)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from datetime import datetime
-from functions import compute_attendance
+from functions import compute_attendance, call_list_sum
  
 attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
 student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
@@ -12,6 +12,7 @@ today = datetime.now()
 ftoday = today.strftime('%d/%m/%y')
 
 compute_attendance(attendance, student_list, ftoday)
+call_list_sum(attendance, student_list)
 
 #print(df)
 #print(df2)
