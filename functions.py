39d1cfa3372bddcc84b8b930ba1e6e2edc4219d7
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-20 10:22:31 -0300

    improving the dateweek function

--- functions.py
+++ functions.py
@@ -47,11 +47,19 @@ def side_bar(attendance, student_list, fdata, side, materias):
 def dateweek(fdata) :
     week_day = fdata.weekday()
     week_start = fdata - datetime.timedelta(days=week_day)
-    week_end = week_start + datetime.timedelta(days=6)
-    temporary_list = [1, 2, 3, 4, 5, 6, 7]
+    week_end = week_start + datetime.timedelta(days=4)
+    temporary_list = [1, 2, 3, 4, 5]
     day = 0
     week_days = []
+    week_days_name = []
     formated_week_days = []
+    translated_names = {
+        "Monday": "Segunda-feira",
+        "Tuesday": "Terça-feira",
+        "Wednesday": "Quarta-feira",
+        "Thursday": "Quinta-feira",
+        "Friday": "Sexta-feira",
+    }
     for n in temporary_list:
         delta = datetime.timedelta(days=day)
         new_date = week_start + delta
@@ -60,3 +68,7 @@ def dateweek(fdata) :
     for data in week_days:
         especific_week_day = data.strftime('%d/%m/%y')
         formated_week_days.append(especific_week_day)
+    for day_name in week_days:
+        name_of_day = day_name.strftime("%A")
+        name_in_portuguese = translated_names.get(name_of_day)
+        week_days_name.append(name_in_portuguese)
