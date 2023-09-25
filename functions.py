d78353a5379febabfdd590e4f73b74c69f2bde98
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-25 09:59:34 -0300

    adding the correct date in the columns

--- functions.py
+++ functions.py
@@ -2,24 +2,60 @@ import pandas as pd
 import streamlit as st
 import datetime as datetime
 
-def compute_attendance(attendance, student_list, fdata, materias):
+def compute_attendance(attendance, student_list, fdata, materias, dateweek, today, lista):
     for index, row in student_list.iterrows():
         id = row['student_id']
         first_name = row['first_name']
         last_name = row['last_name']
-        column1, column2 = st.columns(2)
+        column1, column2, column3, column4, column5, column6 = st.columns(6)
         with column1:
             st.markdown(f"### {first_name} {last_name}")
 
-        with column2:
-            if st.button("Presente", key=f'{index}_presente'):
-                row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [fdata], 'subject': [materias]})
-                attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+            with column2:
+                if st.button("Presente", key=f'{index}_presente'):
+                    row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [lista[0]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
 
-            if st.button("Ausente", key=f'{index}_ausente'):
-                row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [fdata], 'subject': [materias]})
-                attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
-        st.markdown('---')
+                if st.button("Ausente", key=f'{index}_ausente'):
+                    row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [lista[0]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+            with column3:
+                if st.button("Presente", key=f'{index}_presente' * 11):
+                    row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [lista[1]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+                if st.button("Ausente", key=f'{index}_ausente' * 11):
+                    row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [lista[1]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+            with column4:
+                if st.button("Presente", key=f'{index}_presente' * 13):
+                    row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [lista[2]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+                if st.button("Ausente", key=f'{index}_ausente' * 13):
+                    row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [lista[2]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+            with column5:
+                if st.button("Presente", key=f'{index}_presente' * 17):
+                    row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [lista[3]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+                if st.button("Ausente", key=f'{index}_ausente' * 17):
+                    row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [lista[3]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+            with column6:
+                if st.button("Presente", key=f'{index}_presente' * 23):
+                    row = pd.DataFrame({'student_id': [id],'attendance': [1],'date': [lista[4]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+
+                if st.button("Ausente", key=f'{index}_ausente' * 23):
+                    row = pd.DataFrame({'student_id': [id], 'attendance': [0],'date': [lista[4]], 'subject': [materias]})
+                    attendance = pd.concat([attendance, row], axis=0, ignore_index=True)
+            st.markdown('---')
     attendance.to_csv('./attendance_table.csv', index=False)
 
 def call_list_sum(attendance, student_list):
@@ -37,10 +73,10 @@ def call_list_sum(attendance, student_list):
     student_list.to_csv('./call_list_students_utf-8.csv', index=False)
     print(id2)
 
-def side_bar(attendance, student_list, fdata, side, materias):
+def side_bar(attendance, student_list, fdata, side, materias, dateweek, today, lista):
     for subject in materias:
         if side == subject:
-            compute_attendance(attendance, student_list, fdata, subject)
+            compute_attendance(attendance, student_list, fdata, subject, dateweek, today, lista)
             if st.button("Contar", key='ola'):
                 call_list_sum(attendance, student_list)
 
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime
 from functions import compute_attendance, call_list_sum, side_bar, dateweek, columns
- 
+
+st.set_page_config (layout="wide")
+
 attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
 student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
 attendance.head()
 student_list.head()
+
+
 
 today = datetime.now()
 ftoday = today.strftime('%d/%m/%y')
 subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
 sidebar = st.sidebar.selectbox("Selecione uma matéria", subject_list)
 
-col1, col2 = st.columns(2)
-col1.title('Alunos')
-col2.title('Presença')
+lista = dateweek(today)
+
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+col1.header('Alunos')
+col2.header(lista[0])
+col3.header(lista[1])
+col4.header(lista[2])
+col5.header(lista[3])
+col6.header(lista[4])
+
 
 with col1:
     st.markdown('')
@@ -24,13 +35,10 @@ with col2:
 
 st.markdown('---')
 
-
-side_bar(attendance, student_list, ftoday, sidebar, subject_list)
+side_bar(attendance, student_list, ftoday, sidebar, subject_list, dateweek, today, lista)
 
 dateweek(today)
 
-lista = dateweek(today)
 
-columns(lista)
 #print(df)
 #print(df2)
