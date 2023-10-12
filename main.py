13ae2e9d2c6c6d96c6b8e2d479a7bf8abb5c2930
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-12 11:25:27 -0300

    making some changes

--- main.py
+++ main.py
@@ -5,27 +5,30 @@ from functions import compute_attendance, call_list_sum, side_bar, dateweek
 
 st.set_page_config (layout="wide")
 
-attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
-student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
-attendance.head()
-student_list.head()
+def call_page() :
+    attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
+    student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
+    attendance.head()
+    student_list.head()
 
-today = datetime.now()
-ftoday = today.strftime('%d/%m/%y')
-subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
-sidebar = st.sidebar.selectbox("Selecione uma matéria", subject_list)
+    today = datetime.now()
+    ftoday = today.strftime('%d/%m/%y')
+    subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
 
-lista = dateweek(today)
 
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+    sidebar = st.sidebar.selectbox("Selecione uma matéria", subject_list)
 
-col1.header('Alunos')
-col2.header(lista[0])
-col3.header(lista[1])
-col4.header(lista[2])
-col5.header(lista[3])
-col6.header(lista[4])
-st.markdown('---')
+    lista = dateweek(today)
 
-side_bar(attendance, student_list, ftoday, sidebar, subject_list, dateweek, today, lista)
-dateweek(today)
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    col1.header('Alunos')
+    col2.header(lista[0])
+    col3.header(lista[1])
+    col4.header(lista[2])
+    col5.header(lista[3])
+    col6.header(lista[4])
+    st.markdown('---')
+
+    side_bar(attendance, student_list, ftoday, sidebar, subject_list, dateweek, today, lista)
+    dateweek(today)
--- page1.py
+++ page1.py
@@ -1,16 +1,31 @@
+import pandas as pd
 import streamlit as st
+from datetime import datetime
+from functions import compute_attendance, call_list_sum, side_bar, dateweek
+from main import call_page
 
-st.markdown("## Selecione uma turma e uma matéria")
 
-column1, column2 = st.columns(2)
+#Reading the files
+attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
+student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
+attendance.head()
+student_list.head()
+
+#Vars
+today = datetime.now()
+ftoday = today.strftime('%d/%m/%y')
+
+#Lists
 serie_list = ['Turma', '1EM A', '1EM B', '2EM A', '2EM B']
-subject_list = ['Matéria', 'Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
+subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
+lista = dateweek(today)
+list1 = ['Ir a lista de chamada']
 
-with column1:
-    select = st.selectbox("Escolha a turma:", serie_list)
+#Page
+st.markdown("## Selecione uma turma")
 
-with column2:
-    select = st.selectbox("Escolha a matéria:", subject_list)
- 
-if st.button("Ir a lista de chamada"):
-    st.markdown("esta funcionando")
+select1 = st.selectbox("Escolha a turma:", serie_list)
+    
+page = st.selectbox("Ir a lista de chamada", list1):
+if page == 'Ir a lista de chamada':    
+    call_page()
