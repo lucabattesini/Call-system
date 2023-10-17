347b85e36c074551793ff69c8d5bb23876933061
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-17 11:07:12 -0300

    creating a side bar to travel to oders pages

--- main.py
+++ main.py
@@ -5,34 +5,42 @@ from functions import compute_attendance, call_list_sum, side_bar, dateweek
 
 st.set_page_config (layout="wide")
 
+page = st.sidebar.radio('escolha', ['menu','seleção'])
 
-attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
-student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
-attendance.head()
-student_list.head()
+def select() :
+    attendance = pd.read_csv("./attendance_table.csv", encoding='UTF-8')
+    student_list = pd.read_csv("./call_list_students_utf-8.csv", encoding="UTF-8")
+    attendance.head()
+    student_list.head()
 
-today = datetime.now()
-ftoday = today.strftime('%d/%m/%y')
-subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
-serie_list = ['1EMA', '2EMA', '3EMA', '1EMB', '2EMB', '3EMB']
+    today = datetime.now()
+    ftoday = today.strftime('%d/%m/%y')
+    subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
+    serie_list = ['1EMA', '2EMA', '3EMA', '1EMB', '2EMB', '3EMB']
 
+    lista = dateweek(today)
 
-st.sidebar.header("Escolha uma matéria e uma turma")
-materia = st.sidebar.selectbox("Matéria", subject_list)
-turma = st.sidebar.selectbox("Turma", serie_list)
-lista = dateweek(today)
+    st.markdown("# Selecione uma turma e uma matéria")
 
-st.markdown("# Selecione uma turma e uma matéria na barra lateral")
+    coluna, coluna1 = st.columns(2)
 
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+    with coluna:
+        materia = st.selectbox("Matéria", subject_list)
+    with coluna1:
+        turma = st.selectbox("Turma", serie_list)
 
-col1.header('Alunos')
-col2.header(lista[0])
-col3.header(lista[1])
-col4.header(lista[2])
-col5.header(lista[3])
-col6.header(lista[4])
-st.markdown('---')
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
 
-side_bar(attendance, student_list, ftoday, materia, subject_list, dateweek, today, lista, turma)
-dateweek(today)
+    col1.header('Alunos')
+    col2.header(lista[0])
+    col3.header(lista[1])
+    col4.header(lista[2])
+    col5.header(lista[3])
+    col6.header(lista[4])
+    st.markdown('---')
+
+    side_bar(attendance, student_list, ftoday, materia, subject_list, dateweek, today, lista, turma)
+    dateweek(today)
+
+if page == 'seleção':
+    select()
