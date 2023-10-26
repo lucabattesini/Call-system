4c8344b131c48b9df55e9c797f907b435e04ec69
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-26 10:14:04 -0300

    changing the name of vars and def in call.py

--- src/call.py
+++ src/call.py
@@ -5,7 +5,7 @@ from functions import compute_attendance, call_list_sum, side_bar, dateweek
 
 st.set_page_config (layout="wide")
 
-def select() :
+def select_class_subject() :
     attendance = pd.read_csv("./db/attendance.csv", encoding='UTF-8')
     student_list = pd.read_csv("./db/student_info_utf-8.csv", encoding="UTF-8")
     attendance.head()
@@ -16,26 +16,26 @@ def select() :
     subject_list = ['Matemática','Inglês','Português','Química','Física','Biologia','Educação','Geografia','História','Sociologia','Filosofia']
     serie_list = ['1EMA', '2EMA', '3EMA', '1EMB', '2EMB', '3EMB']
 
-    lista = dateweek(today)
+    date_week = dateweek(today)
 
     st.markdown("# Selecione uma turma e uma matéria")
 
     coluna, coluna1 = st.columns(2)
 
     with coluna:
-        materia = st.selectbox("Matéria", subject_list)
+        subject = st.selectbox("Matéria", subject_list)
     with coluna1:
-        turma = st.selectbox("Turma", serie_list)
+        student_class = st.selectbox("Turma", serie_list)
 
     col1, col2, col3, col4, col5, col6 = st.columns(6)
 
     col1.header('Alunos')
-    col2.header(lista[0])
-    col3.header(lista[1])
-    col4.header(lista[2])
-    col5.header(lista[3])
-    col6.header(lista[4])
+    col2.header(date_week[0])
+    col3.header(date_week[1])
+    col4.header(date_week[2])
+    col5.header(date_week[3])
+    col6.header(date_week[4])
     st.markdown('---')
 
-    side_bar(attendance, student_list, ftoday, materia, subject_list, dateweek, today, lista, turma)
+    side_bar(attendance, student_list, ftoday, subject, subject_list, dateweek, today, date_week, student_class)
     dateweek(today)
