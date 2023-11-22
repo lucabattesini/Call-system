33dad424e40383e2bdf8b0b895609a7933eec3a2
Clara Battesini <clarabattesini@gmail.com>
2023-11-22 15:03:51 -0500

    New todo's for database connection

--- src/db/connection.py
+++ src/db/connection.py
@@ -5,7 +5,17 @@ attendance_path = os.path.join(os.path.dirname(__file__), 'attendance.csv')
 students_path = os.path.join(os.path.dirname(__file__), 'students_utf-8.csv')
 
 def get_attendance_df() :
+    # TODO Verify if the file exists if not create them using the os lib
     return pd.read_csv(attendance_path, encoding="UTF-8")
 
 def get_students_df() :
+    # TODO Verify if the file exists if not create them using the os lib
     return pd.read_csv(students_path, encoding="UTF-8")
+
+def save_attendance_df():
+    # TODO Implement a function that receive a df and save it
+    pass
+
+def save_students_df():
+    # TODO Implement a function that receive a df and save it
+    pass
