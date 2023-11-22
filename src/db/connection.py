994fa650e465f8bf8b99bcbed80c70d90301255f
Clara Battesini <clarabattesini@gmail.com>
2023-11-22 15:39:14 -0500

    Fix file paths and add save functions for attendance and students data

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
 
-attendance_path = os.path.join(os.path.dirname(__file__), 'attendance.csv')
-students_path = os.path.join(os.path.dirname(__file__), 'students_utf-8.csv')
+db_dir = os.path.join(os.path.dirname(__file__))
+attendance_path = os.path.join(db_dir, 'attendance.csv')
+students_summary_path = os.path.join(db_dir, 'students_summary.csv')
+students_path = os.path.join(db_dir, 'students.csv')
 
 def get_attendance_df() :
     # TODO Verify if the file exists if not create them using the os lib
@@ -12,10 +14,11 @@ def get_students_df() :
     # TODO Verify if the file exists if not create them using the os lib
     return pd.read_csv(students_path, encoding="UTF-8")
 
-def save_attendance_df():
-    # TODO Implement a function that receive a df and save it
-    pass
+def save_attendance_df(df):
+    df.to_csv(attendance_path, index=False)
 
-def save_students_df():
-    # TODO Implement a function that receive a df and save it
-    pass
+def save_students_df(df):
+    df.to_csv(students_path, index=False)
+
+def save_students_summary_df(df):
+    df.to_csv(students_summary_path, index=False)
