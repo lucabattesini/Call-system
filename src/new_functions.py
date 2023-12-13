75422ad8640b10087282192ea3f23a9d6549a72a
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-12-13 11:24:16 -0300

    creating a function that return the percentage of presences

--- src/new_functions.py
+++ src/new_functions.py
@@ -9,3 +9,10 @@ def get_student_presence_sum(id) :
         attendance_total = df['attendance_total']
         if student_id == id:
             return attendance_total
+
+def get_student_presence_and_absence(absences, id) :
+    presences = get_student_presence_sum(id)
+    all_classes = presences + absences
+    presences_first_part_formula = presences * 100
+    presences_percentage = presences_first_part_formula / all_classes
+    return all_classes, presences_percentage
