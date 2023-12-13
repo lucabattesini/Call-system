22326c60c28ea691fdc35e3332397f6b4b8e6d79
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-12-13 11:11:05 -0300

    creating a function that get the attendance total of presences

--- src/new_functions.py
+++ src/new_functions.py
@@ -1,3 +1,11 @@
 import pandas as pd 
 import streamlit as st
+from db.connection import get_students_df
 
+def get_student_presence_sum(id) :
+    df = get_students_df()
+    for _ in df.iterrows():
+        student_id = df['student_id']
+        attendance_total = df['attendance_total']
+        if student_id == id:
+            return attendance_total
