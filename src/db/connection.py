19cdf87775873f29d8bafaf9c2abbc648cbf5ccb
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-12-14 11:58:46 -0300

    fixing the file that read the notes file

--- src/db/connection.py
+++ src/db/connection.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 db_dir = os.path.join(os.path.dirname(__file__))
 attendance_path = os.path.join(db_dir, 'attendance.csv')
-notes_path = attendance_path = os.path.join(db_dir, 'notes.csv')
+notes_path = os.path.join(db_dir, 'notes.csv')
 students_summary_path = os.path.join(db_dir, 'students_summary.csv')
 students_path = os.path.join(db_dir, 'students.csv')
 
 def get_attendance_df() :
     # TODO Verify if the file exists if not create them using the os lib
     return pd.read_csv(attendance_path, encoding="UTF-8")
+
+def get_notes_df() :
+    # TODO Verify if the file exists if not create them using the os lib
+    return pd.read_csv(notes_path, encoding="UTF-8")
 
 def get_students_df() :
     # TODO Verify if the file exists if not create them using the os lib
