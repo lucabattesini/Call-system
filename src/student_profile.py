9f734136ac015cd8ad52e811881cc8ee68b8db49
Luca Battesini <lucabattesini1@gmail.com>
2023-12-19 21:08:00 +0100

    creating a function that save a list of students of a class x

--- src/student_profile.py
+++ src/student_profile.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from db.connection import get_students_df
 
 def show_profile(first_name, last_name, student_class) :
     column, column2 = st.columns(2)
@@ -12,9 +13,20 @@ def show_profile(first_name, last_name, student_class) :
 # --- READ (get)
 
 def get_students(classId):
+    df = get_students_df()
+    students_from_class = []
+    for i, row in df.iterrows:
+        year =  row['school_year']
+        classroom = row['classroom']
+        class_name = year + classroom
+        first_name = row['first_name']
+        last_name = row['last_name']
+        name = first_name + last_name
+        if classId == class_name:
+            students_from_class.append(name)
     # procura todos os estudantes de uma turma específica
     # retorna a lista de estudantes
-    return []
+    return students_from_class
 
 def get_student(id):
     student = { 'name': "", 'grade': 0, 'attendance': [], 'total_attendance_per_class': []}
