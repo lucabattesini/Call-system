8cd3a7d8a24c083eda4000e9bb390f0218bd836b
Luca Battesini <lucabattesini1@gmail.com>
2023-12-20 15:26:36 +0100

    Creating a function that create a Series of a student information

--- src/student_profile.py
+++ src/student_profile.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from db.connection import get_students_df
+from db.connection import get_students_df, get_attendance_df
 
 def show_profile(first_name, last_name, student_class) :
     column, column2 = st.columns(2)
@@ -29,7 +29,27 @@ def get_students(classId):
     return students_from_class
 
 def get_student(id):
+    students = get_students_df
+    attendance = get_attendance_df
     student = { 'name': "", 'grade': 0, 'attendance': [], 'total_attendance_per_class': []}
+    for row in student.iterrows():
+        student_id = row['student_id']
+        first_name = row['first_name']
+        last_name = row['last_name']
+        name = first_name + last_name
+        year = row['school_year']
+        classes = row['classroom']
+        classroom = year + classes
+        if student_id == id:
+            for row2 in attendance.iterrows():
+                    student_id2 = row2['student_id']
+                    attendance = row2['attendance']
+                    attendance_total = 0
+                    if student_id2 == id:
+                        if attendance == 1:
+                            attendance_total += 1
+            student = { 'name': name, 'grade': classroom, 'attendance': attendance_total, 'total_attendance_per_class': []}                       
+                    
     # procura o estudante com o id passado
     # preenche no dicionário student os campos certos
     return student
