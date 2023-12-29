1d4f4f27ad897962509109fb4f843dc8e8665462
Luca Battesini <lucabattesini1@gmail.com>
2023-12-29 20:18:04 -0300

    doing the function search with the name of the student

--- src/search_functions.py
+++ src/search_functions.py
@@ -2,15 +2,17 @@ import streamlit as st
 from student_profile import show_profile
 from db.connection import get_students_df
 
-def student_search(id) :
+def student_search(name) :
     df  = get_students_df()
     for _, row in df.iterrows():
-            student_id = row['student_id']
-            student_first_name = row['first_name']
-            student_last_name = row['last_name']
-            year_class = row['school_year']
-            class_name = row['classroom']
-            class_full_name = year_class + class_name
-
-            if student_id == id:
+        student_id = row['student_id']
+        student_first_name = row['first_name']
+        student_last_name = row['last_name']
+        student_name = student_first_name + " " + student_last_name
+        year_class = row['school_year']
+        class_name = row['classroom']
+        class_full_name = year_class + class_name
+        student_name_lower = student_name.lower()
+        input_student_name_lower = name.lower()
+        if student_name_lower == input_student_name_lower:
                     show_profile(student_first_name, student_last_name, class_full_name)
