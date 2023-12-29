773b3811fc6c62d6a552fe5052e1eb28754744c8
Luca Battesini <lucabattesini1@gmail.com>
2023-12-29 17:37:18 -0300

    correcting the student search function

--- src/search_functions.py
+++ src/search_functions.py
@@ -1,20 +1,14 @@
 import streamlit as st
 from student_profile import show_profile
 
-def student_search(student_list, student_class, student) :
+def student_search(student_list, id) :
     for _, row in student_list.iterrows():
             student_id = row['student_id']
-            class_number = row['school_year']
-            class_letter = row['classroom']
-            class_fullname = class_number + class_letter
             student_first_name = row['first_name']
             student_last_name = row['last_name']
-            if class_fullname == student_class:
-                if student == '':
-                      if st.button(student_first_name, key=student_id):
-                            show_profile(student_first_name, student_last_name, class_fullname)
-                
-                elif student_first_name.lower() == student:
-                        if st.button(student_first_name, key=student_id):
-                            st.markdown("Funcionou")
-                            show_profile(student_first_name, student_last_name, class_fullname)
+            year_class = row['school_year']
+            class_name = row['classroom']
+            class_full_name = year_class + class_name
+
+            if student_id == id:
+                    show_profile(student_first_name, student_last_name, class_full_name)
