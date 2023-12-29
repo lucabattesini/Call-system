558fa863930e0aa8befe0368d8ae4e9fd171e841
Luca Battesini <lucabattesini1@gmail.com>
2023-12-29 20:22:37 -0300

    fixing a grammar error

--- src/search.py
+++ src/search.py
@@ -6,6 +6,6 @@ from db.connection import get_students_df
 
 def search_profile() :
 
-    study_search = st.text_input("Insira o número de matricula do estudante")
+    study_search = st.text_input("Insira o nome completo do aluno")
 
     student_search(study_search)
