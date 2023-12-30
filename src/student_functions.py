32c7a925cffc77588aae678667cb2b16100491df
Luca Battesini <lucabattesini1@gmail.com>
2023-12-30 10:19:13 -0300

    creating a visual for the get_presence_vy_subject function

--- src/student_functions.py
+++ src/student_functions.py
@@ -1,7 +1,8 @@
 import pandas as pd 
+import streamlit as st
 from datetime import datetime
 from db.connection import get_attendance_df, save_notes_df, get_notes_df
-from utils import dateweek
+from utils import dateweek, subjects_list
 
 def get_student_presence_sum(id) :                                                            
     df = get_attendance_df()
@@ -67,9 +68,10 @@ def get_student_presence_and_absence_by_week(id) :
     presences_percentage = presences_first_part_formula / all_classes
     return all_classes, presences_percentage
 
-def get_presence_by_subject(subject, id) :
+def get_presence_by_subject(id) :
     df = get_attendance_df()
-    student_presence = []
+    subject = st.selectbox("Matéria", subjects_list)
+    column, column2 = st.columns(2)
     for _, row in df.iterrows():
         student_id = row['student_id']
         presence_subject = row['subject']
@@ -78,12 +80,16 @@ def get_presence_by_subject(subject, id) :
         if student_id == id:
             if presence_subject == subject:
                 if attendance == 1:
-                    date_and_subject = str(f"Presente\n Matéria: {subject}\n Data: {date}")
-                    student_presence.append(date_and_subject)
+                    with column:
+                        st.markdown(f"### Presente")
+                    with column2:
+                        st.markdown(f"### Data: {date}")
+
                 elif attendance == 0:
-                    date_and_subject = str(f"Ausente\n Matéria: {subject}\n Data: {date}")
-                    student_presence.append(date_and_subject)
-    return student_presence
+                    with column:
+                        st.markdown(f"### Ausente")
+                    with column2:
+                        st.markdown(f"### Data: {date}")
 
 def create_student_note(note, subject, id) :
     df = get_notes_df()
--- src/student_profile.py
+++ src/student_profile.py
@@ -5,9 +5,9 @@ from db.connection import get_students_df, get_attendance_df
 def show_profile(student_name, student_class) :
     column, column2 = st.columns(2)
     with column:
-        st.markdown(f'# {student_name}')
+        st.markdown(f'### {student_name}')
     with column2:
-        st.markdown(f'# {student_class}')
+        st.markdown(f'### {student_class}')
 
 # --- READ (get)
 
