00cb03f919ea7e794c82e822416fb31ed74ed83c
Luca Battesini <lucabattesini1@gmail.com>
2023-12-30 10:33:51 -0300

    fixing an error

--- src/search.py
+++ src/search.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import streamlit as st
 from search_functions import student_search
-from utils import classes_list
-from db.connection import get_students_df
 
 def search_profile() :
 
