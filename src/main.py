f8e4f4dcc93922c7a180befb01b57ba1401aab74
Luca Battesini <lucabattesini1@gmail.com>
2024-01-07 11:15:44 -0300

    DEleting the notes function and creating to create oother one

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ import streamlit as st
 from search import search_profile
 from call import select_class_subject
 from menu import menu
-from notes import student_search
-from utils import subjects_list
 
-page = st.sidebar.radio('Paginas', ['Menu','Chamada', 'Perfil do aluno',  'Anotações'])
+page = st.sidebar.radio('Paginas', ['Menu','Chamada', 'Perfil do aluno'])
 
 if page == 'Menu':
     menu()
@@ -15,8 +13,3 @@ if page == 'Chamada':
 
 if page == 'Perfil do aluno':
     search_profile()
-
-if page == 'Anotações':
-    subject = st.selectbox('Escolha uma matéria', subjects_list)
-    name = st.text_input("Nome do aluno")
-    student_search(name)
