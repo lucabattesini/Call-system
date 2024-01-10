8b0760946d7969ad6bce3523344212336f244f8b
Luca Battesini <lucabattesini1@gmail.com>
2024-01-10 12:48:54 -0300

    Creating the graphic function

--- src/graphic_functions.py
+++ src/graphic_functions.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-from student_functions import get_student_presence_and_absence_by_week
+from student_functions import get_student_presence_by_day
 
-def create_a_graphic_divided_by_week() :
-    df = pd.DataFrame(lista, columns = ["Presença"])
-    st.line_chart(df, y="Presença")
+def create_a_graphic_divided_by_week(id) :
+    presence_list_by_day = get_student_presence_by_day(id)
+    df = pd.DataFrame(presence_list_by_day, columns = ["Presença"])
+    return st.line_chart(df, y="Presença")
