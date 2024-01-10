f62dbc3d9983f1005085511fb4c73a87322eeda4
Luca Battesini <lucabattesini1@gmail.com>
2024-01-09 23:09:23 -0300

    Starting to creating the line chart function

--- src/graphic_functions.py
+++ src/graphic_functions.py
@@ -1,2 +1,7 @@
 import streamlit as st
-from student_functions import get_student_presence_and_absence
+import pandas as pd
+from student_functions import get_student_presence_and_absence_by_week
+
+def create_a_graphic_divided_by_week() :
+    df = pd.DataFrame(lista, columns = ["Presença"])
+    st.line_chart(df, y="Presença")
