539ed8a955c0d320a91871de7ff5081d7f9136a0
Luca Battesini <lucabattesini1@gmail.com>
2024-01-11 20:00:48 -0300

    finishing the graphic function

--- src/graphic_functions.py
+++ src/graphic_functions.py
@@ -2,7 +2,25 @@ import streamlit as st
 import pandas as pd
 from student_functions import get_student_presence_by_day
 
+def presence_list_by_day_function(id) :
+    presence_list = get_student_presence_by_day(id)
+    presença_dia = 0
+    presence_list_by_day  = []
+    ct = 0
+    for presence in presence_list:
+        ct += 1
+        if ct < 5:
+            if presence == 1:
+                presença_dia += 1
+        elif ct == 5:
+            ct = 0
+            if presence == 1:
+                presença_dia +=1
+            presence_list_by_day.append(presença_dia)
+            presença_dia = 0
+    return presence_list_by_day
+
 def create_a_graphic_divided_by_week(id) :
-    presence_list_by_day = get_student_presence_by_day(id)
-    df = pd.DataFrame(presence_list_by_day, columns = ["Presença"])
+    presences_by_day = presence_list_by_day_function(id)
+    df = pd.DataFrame(presences_by_day, columns = ["Presença"])
     return st.line_chart(df, y="Presença")
