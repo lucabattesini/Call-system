5c78784962935a614ec1528d4c3769355c57d3c5
Luca Battesini <lucabattesini1@gmail.com>
2024-01-12 12:46:46 -0300

    Improving tthe design of abcences area

--- src/search_functions.py
+++ src/search_functions.py
@@ -21,9 +21,9 @@ def student_search(name) :
                 show_profile(student_name, class_full_name)
                 get_presence_percent_bar(student_id)
                 create_a_graphic_divided_by_day(student_id)
+                st.title("Ultimas 5 faltas")
                 get_absences_by_student(student_id)
-                st.markdown("""---""")
-                with st.form("my form"):
+                with st.form("Notes"):
                     note = st.text_input("Adicione uma nova anotação") 
                     save = st.form_submit_button("Salvar")
                     if save:    
