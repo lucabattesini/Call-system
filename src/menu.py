3b34f6e599f8fbfdb4720ccd3f22537b911586cb
Luca Battesini <lucabattesini1@gmail.com>
2024-01-23 11:48:27 -0300

    deleting the line from menu.py

--- src/menu.py
+++ src/menu.py
@@ -3,4 +3,3 @@ import streamlit as st
 def menu() :
     st.markdown("# Projeto de chamadas escolares")
     st.markdown('''**Este projeto de programação foi criado com um único propósito: aprendizado. Cada linha de código e desafio enfrentado, tem como objetivo aprimorar minhas habilidades e aplicar os conhecimentos teóricos adquiridos. É crucial esclarecer que este projeto não tem intenções lucrativas. Ao compartilhar publicamente este projeto, espero inspirar outros a iniciarem suas próprias jornadas de aprendizado, motivados pelo amor à programação e pelo constante desejo de evoluir tecnicamente.**''')
-    st.markdown("---")
